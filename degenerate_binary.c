d093a2023a2f313c86e1831640411968f1e49f75
RAKSHITADODDAMANI <130588095+RAKSHITADODDAMANI@users.noreply.github.com>
2024-04-25 14:55:45 +0530

    Add files via upload

---
+++ degenerate_binary.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#define count 10
+
+struct node{
+     int data;
+     struct node *left,*right;
+};
+
+struct node * newnode(int data)
+{
+    struct node * node=(struct node *)malloc(sizeof(struct node));
+    node->data=data;
+    node->left=node->right=NULL;
+    return node;
+}
+//it does reverse inorder
+void print2DUtil(struct node * root ,int space)
+{
+    if(root==NULL)
+        return;
+    //increase the dist b\w levels
+    space+=count;
+
+    // process right child first
+    print2DUtil(root->right,space);
+    //print current node after space
+    //count
+    printf("\n");
+    for(int i=count;i<space;i++)
+        printf(" ");
+    printf("%d\n",root->data);
+    //process left child
+    print2DUtil(root->left,space);
+}
+
+//wrapper over print2DUtil()
+void print2D(struct node* root)
+{
+    //initial space count as 0
+    print2DUtil(root,0);
+}
+
+//Driver program to test above functions
+int main()
+{
+    struct node * root=newnode(1);
+    root->left=newnode(2);
+
+    root->left->right=newnode(5);
+
+
+    print2D(root);
+    return 0;
+}
